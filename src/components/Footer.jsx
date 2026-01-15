5c66b4b4f7850aa7798a8fa7d5d452ebc3857e72
Íthan P. Amaral <ithanamaral@gmail.com>
2026-01-15 15:50:54 -0300

    limpa footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,15 @@
-import {
-  Heart,
-  MapPin,
-  Phone,
-  Mail,
-  Facebook,
-  Instagram,
-  Youtube,
-} from "lucide-react";
+
 import "./Footer.css";
 
 const Footer = () => {
-  const quickLinks = [
-    { name: "Início", href: "#inicio" },
-    { name: "Sobre Nós", href: "#sobre" },
-    { name: "Cultos", href: "#cultos" },
-    { name: "Ministérios", href: "#ministerios" },
-    { name: "Contato", href: "#contato" },
-  ];
-
-  const ministries = [
-    { name: "Ministério de Jovens", href: "#ministerios" },
-    { name: "Ministério Pastoral", href: "#ministerios" },
-    { name: "Ministério de Louvor", href: "#ministerios" },
-    { name: "Ministério Social", href: "#ministerios" },
-    { name: "Escola Bíblica", href: "#ministerios" },
-  ];
-
-  return (
+    return (
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-grid">
          <img src="./logo2.png" alt="logo do Hanouer" className="footer-logo" />
         </div>
 
-        {/* Bottom Section */}
+  
         <div className="footer-bottom">
           <div className="footer-bottom-content">
             <div className="footer-copyright">
